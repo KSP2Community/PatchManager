b750967a2714b9b008912a7e59f5355438b117da
Lexi <arallen3660@gmail.com>
2023-06-10 18:45:21 -0400

    Set up a simple patch tester project for testing patches standalone

---
+++ StandalonePatchTester/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if (Directory.Exists("json") && Directory.Exists("patches"))
+{
+    if (Directory.Exists("patched"))
+    {
+        Directory.Delete("patched",true);
+    }
+}
+else
+{
+    Console.WriteLine("Usage: run this program in a folder that has a json/ subfolder and a patches/ subfolder and it will run every patch in the patches folder and output patched files into patched/");
+}
