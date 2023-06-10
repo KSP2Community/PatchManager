dac2fda15a049267cc4ebacfc0a1b8c51a23835d
Lexi <arallen3660@gmail.com>
2023-06-10 19:01:10 -0400

    Starting standalone testing of the patch runner

--- StandalonePatchTester/Program.cs
+++ StandalonePatchTester/Program.cs
@@ -1,11 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using PatchManager.SassyPatching.Execution;
+using PatchManager.Shared.Interfaces;
+
 if (Directory.Exists("json") && Directory.Exists("patches"))
 {
     if (Directory.Exists("patched"))
     {
         Directory.Delete("patched",true);
     }
+
+    List<ITextPatcher> patchers = new();
+    var universe = new Universe(patchers.Add, Console.WriteLine);
+    universe.LoadPatchesInDirectory(new DirectoryInfo("patches"), "test");
+
+    Console.WriteLine($"{patchers.Count} patcher(s) registered!");
 }
 else
 {
--- src/PatchManager.SassyPatching/Execution/Universe.cs
+++ src/PatchManager.SassyPatching/Execution/Universe.cs
@@ -1,6 +1,9 @@
-﻿using PatchManager.SassyPatching.Attributes;
+﻿using Antlr4.Runtime;
+using PatchManager.SassyPatching.Attributes;
 using PatchManager.SassyPatching.Interfaces;
+using PatchManager.SassyPatching.Nodes;
 using PatchManager.Shared.Interfaces;
+using SassyPatchGrammar;
 
 namespace PatchManager.SassyPatching.Execution;
 
@@ -57,13 +60,98 @@ public class Universe
     public readonly Action<ITextPatcher> RegisterPatcher;
 
     /// <summary>
+    /// This logs errors in this universe
+    /// </summary>
+    public readonly Action<string> ErrorLogger;
+
+    /// <summary>
     /// Create a new universal state
     /// </summary>
     /// <param name="registerPatcher">This action receives patchers and registers them for later execution</param>
-    public Universe(Action<ITextPatcher> registerPatcher)
+    public Universe(Action<ITextPatcher> registerPatcher, Action<string> errorLogger)
     {
         RegisterPatcher = registerPatcher;
+        ErrorLogger = errorLogger;
     }
 
+    // TODO: Fix this so that other mods stages get their guids working
     public Dictionary<string,PatchLibrary> AllLibraries = new(AllManagedLibraries);
+
+    private class LoadException : Exception
+    {
+        public LoadException(string message) : base(message)
+        {
+        }
+    }
+
+    private class LoadListener : IAntlrErrorListener<IToken>
+    {
+        internal static readonly LoadListener Instance = new();
+
+        private LoadListener()
+        {
+            
+        }
+        public void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine,
+            string msg, RecognitionException e)
+        {
+            throw new LoadException($"{line}:{charPositionInLine}: msg");
+        }
+    }
+
+    /// <summary>
+    /// Loads all patches from a directory
+    /// </summary>
+    /// <param name="directory"></param>
+    /// <param name="modId">The ID of the mod to load the guid as</param>
+    public void LoadPatchesInDirectory(DirectoryInfo directory, string modId)
+    {
+        
+        var tokenTransformer = new Transformer(msg => throw new LoadException(msg));
+        foreach (var library in directory.EnumerateFiles("_*.patch", SearchOption.AllDirectories))
+        {
+            string name = modId + ":" + library.Name.Replace(".patch", "").TrimFirst();
+            try
+            {
+                var charStream = CharStreams.fromPath(library.FullName);
+                var lexer = new sassy_lexer(charStream);
+                var tokenStream = new CommonTokenStream(lexer);
+                var parser = new sassy_parser(tokenStream);
+                parser.AddErrorListener(LoadListener.Instance);
+                var patchContext = parser.patch();
+                tokenTransformer.Errored = false;
+                var patch = tokenTransformer.Visit(patchContext) as SassyPatch;
+                var lib = new SassyPatchLibrary(patch);
+                AllLibraries[name] = lib;
+            }
+            catch (Exception e)
+            {
+                ErrorLogger($"Could not load library: {name} due to: {e.Message}");
+            }
+        }
+
+        foreach (var patch in directory.EnumerateFiles("*.patch", SearchOption.AllDirectories))
+        {
+            if (patch.Name.StartsWith("_")) continue;
+            try
+            {
+                var charStream = CharStreams.fromPath(patch.FullName);
+                var lexer = new sassy_lexer(charStream);
+                var tokenStream = new CommonTokenStream(lexer);
+                var parser = new sassy_parser(tokenStream);
+                parser.AddErrorListener(LoadListener.Instance);
+                var patchContext = parser.patch();
+                tokenTransformer.Errored = false;
+                var gEnv = new GlobalEnvironment(this,modId);
+                var env = new Environment(gEnv);
+                var ctx = tokenTransformer.Visit(patchContext) as SassyPatch;
+                ctx?.ExecuteIn(env);
+                // lib = new SassyPatchLibrary(patch);
+            }
+            catch (Exception e)
+            {
+                ErrorLogger($"Could not run patch: {modId}:{patch.Name} due to: {e.Message}");
+            }
+        }
+    }
 }
