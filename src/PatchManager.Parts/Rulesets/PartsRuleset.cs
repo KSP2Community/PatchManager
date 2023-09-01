df020634f41b3e203932f952cc87de63cbbacc29
Lexi <arallen3660@gmail.com>
2023-09-01 12:00:15 -0400

    Start towards allowing the creation of parts

--- src/PatchManager.Parts/Rulesets/PartsRuleset.cs
+++ src/PatchManager.Parts/Rulesets/PartsRuleset.cs
@@ -1,7 +1,16 @@
-﻿using PatchManager.Parts.Selectables;
+﻿using KSP.Game;
+using KSP.IO;
+using KSP.OAB;
+using KSP.Sim;
+using KSP.Sim.Definitions;
+using KSP.Sim.ResourceSystem;
+using Newtonsoft.Json.Linq;
+using PatchManager.Parts.Selectables;
 using PatchManager.SassyPatching;
 using PatchManager.SassyPatching.Attributes;
 using PatchManager.SassyPatching.Interfaces;
+using PatchManager.SassyPatching.NewAssets;
+using UnityEngine;
 
 namespace PatchManager.Parts.Rulesets;
 
@@ -26,5 +35,75 @@ public class PartsRuleset : IPatcherRuleSet
         return new PartSelectable(jsonData);
     }
 
-    public INewAsset CreateNew(List<DataValue> dataValues) => throw new NotImplementedException();
+    /// <summary>
+    /// Create a new part asset
+    /// </summary>
+    /// <param name="dataValues">The arguments for the part asset, only one argument: name</param>
+    /// <returns>The part asset creator</returns>
+    public INewAsset CreateNew(List<DataValue> dataValues)
+    {
+        var name = dataValues[0].String;
+        var internalPartData = new PartData
+        {
+            partName = name,
+            angularDrag = 0,
+            coMassOffset = Vector3.zero,
+            coPressureOffset = Vector3.zero,
+            mass = 0,
+            maximumDrag = 0,
+            maxTemp = 0,
+            author = "",
+            bodyLiftOnlyUnattachedLift = false,
+            bodyLiftOnlyAttachName = "",
+            buoyancy = 0,
+            buoyancyUseSine = false,
+            breakingForce = 0,
+            breakingTorque = 0,
+            category = PartCategories.none,
+            family = "",
+            coBuoyancy = Vector3.zero,
+            coDisplacement = Vector3.zero,
+            oabEditorCategory = OABEditorPartCategory.NONE,
+            partType = AssemblyPartTypeFilter.Rocket,
+            cost = 0,
+            crashTolerance = 0,
+            crewCapacity = 0,
+            emissiveConstant = 0,
+            explosionPotential = 0,
+            fuelCrossFeed = false,
+            heatConductivity = 0,
+            inverseStageCarryover = false,
+            isCompound = false,
+            maxLength = 0,
+            radiatorHeadroom = 0,
+            radiatorMax = 0,
+            physicsMode = PartPhysicsModes.None,
+            sizeCategory = MetaAssemblySizeFilterType.XS,
+            skinMassPerArea = 0,
+            skinMaxTemp = 0,
+            skinInternalConductionMult = 0,
+            stageOffset = 0,
+            stageType = AssemblyPartStageType.None,
+            tags = "",
+            stagingIconAssetAddress = "",
+            attachRules = AttachRules.Defaults(),
+            attachNodes = new List<AttachNodeDefinition>(),
+            resourceContainers = new List<ContainedResourceDefinition>(),
+            resourceCosts = new List<PartResourceCostDefinition>(),
+            serializedPartModules = new List<SerializedPartModule>(),
+            resourceSummary = new SerializedResourceInfo(),
+            PAMModuleSortOverride = new List<PartsManagerCore.SerializedPartModuleDisplayOrder>(),
+            PAMModuleVisualsOverride = new List<PartsManagerCore.SerializedPartModuleDisplayVisuals>(),
+            AllowKinematicPhysicsIfIntersectTerrain = false
+        };
+        var internalJson = IOProvider.ToJson(internalPartData);
+        var internalJObject = JObject.Parse(internalJson);
+        var externalJObject = new JObject
+        {
+            ["version"] = "0.3",
+            ["useExternalData"] = false,
+            ["data"] = internalJObject
+        };
+        return new NewGenericAsset("parts_data", name, new PartSelectable(externalJObject.ToString()));
+    }
 }
