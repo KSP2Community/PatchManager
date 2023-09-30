813ca9ee38c8830da685949c08a21917957b8a1c
Lexi <arallen3660@gmail.com>
2023-09-30 12:22:13 -0400

    Remove unused planets dll from being loaded

--- src/PatchManager/PatchManagerPlugin.cs
+++ src/PatchManager/PatchManagerPlugin.cs
@@ -45,7 +45,7 @@ public class PatchManagerPlugin : BaseSpaceWarpPlugin
         ModuleManager.Register(Path.Combine(dir.FullName, "PatchManager.Parts.dll"));
         ModuleManager.Register(Path.Combine(dir.FullName, "PatchManager.Generic.dll"));
         ModuleManager.Register(Path.Combine(dir.FullName, "PatchManager.Resources.dll"));
-        ModuleManager.Register(Path.Combine(dir.FullName, "PatchManager.Planets.dll"));
+        // ModuleManager.Register(Path.Combine(dir.FullName, "PatchManager.Planets.dll"));
     }
 
     private void Start()
